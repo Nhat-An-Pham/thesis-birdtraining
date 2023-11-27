be48e0083af64af8c1608149a92ef80f82ddc96e
Bao <nguyenthothaibao@gmail.com>
2023-11-27 13:45:52 +0700

    Dmm mmay Trung

--- src/Management/customer/TrainerFinishTicketView.jsx
+++ src/Management/customer/TrainerFinishTicketView.jsx
@@ -14,6 +14,7 @@ import {
   Toolbar,
   Typography,
   Table,
+  Paper,
 } from "@mui/material";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -131,7 +132,7 @@ const TrainerFinishTicketView = ({
         </Toolbar>
       </AppBar>
 
-      <TableContainer>
+      <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableCell>
