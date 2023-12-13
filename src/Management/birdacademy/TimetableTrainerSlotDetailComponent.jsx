7834b5bfb34a769450e0c9cbe041c0502836ea47
quandoivietnam <trinhnamanh0909@gmail.com>
2023-12-13 13:05:59 +0700

    fix ui trainer/ staff can't see trainer slot detail

--- src/Management/birdacademy/TimetableTrainerSlotDetailComponent.jsx
+++ src/Management/birdacademy/TimetableTrainerSlotDetailComponent.jsx
@@ -29,11 +29,15 @@ import trainingCourseManagementService from "../../services/trainingcourse-manag
 import { ochreTheme } from "../themes/Theme";
 import { ToastContainer, toast } from "react-toastify";
 import SkillDoneDialog from "./SkillDoneDialog";
+import { jwtDecode } from "jwt-decode";
 
 const TimetableTrainerSlotDetailComponent = ({
   trainerSlotId,
   callBackTimetable,
 }) => {
+  const userRole = jwtDecode(
+    JSON.parse(localStorage.getItem("user-token"))
+  ).role;
   const [renderDialog, setRenderDialog] = useState(0);
 
   const [slotList, setSlotList] = useState([]);
@@ -200,7 +204,7 @@ const TimetableTrainerSlotDetailComponent = ({
                 />
               </Grid>
             </Grid>
-            {timetableDetail.status == "NotYet" && (
+            {timetableDetail.status == "NotYet" && userRole == "Trainer" && (
               <ThemeProvider theme={ochreTheme}>
                 <Button
                   variant="contained"
