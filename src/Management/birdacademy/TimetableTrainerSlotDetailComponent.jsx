ec336c0cc0171bf30baab6b0e5dbc259905c6430
DThongLeon <hoangthong432001@gmail.com>
2023-12-13 10:03:17 +0700

    fix ui trainer

--- src/Management/birdacademy/TimetableTrainerSlotDetailComponent.jsx
+++ src/Management/birdacademy/TimetableTrainerSlotDetailComponent.jsx
@@ -99,7 +99,7 @@ const TimetableTrainerSlotDetailComponent = ({
     callBackTimetable();
   }
   return (
-    <ThemeProvider padding={20} theme={ochreTheme}>
+    <ThemeProvider theme={ochreTheme}>
       {renderDialog == 1 && (
         <SkillDoneDialog
           trainingProgressId={timetableDetail?.progressId}
@@ -108,7 +108,11 @@ const TimetableTrainerSlotDetailComponent = ({
         />
       )}
       <ToastContainer />
-      <AppBar position="static" color="ochre">
+      <AppBar
+        position="static"
+        color="ochre"
+        sx={{ borderBottomRightRadius: 20, borderBottomLeftRadius: 20 }}
+      >
         <Toolbar>
           <IconButton
             size="large"
@@ -123,94 +127,181 @@ const TimetableTrainerSlotDetailComponent = ({
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "block" },
+              fontWeight: 700,
+            }}
           >
             Training Course Detail
           </Typography>
         </Toolbar>
       </AppBar>
-      <Divider />
-      <div style={{ margin: "50px" }}>
+      <div>
         {timetableDetail != null && (
-          <div>
-            <Grid container spacing={1}>
-              {slotList != null &&
-                slotList
-                  .filter((cls) => cls.id == timetableDetail.slotId)
-                  .map((cls) => (
-                    <>
-                      <Grid item xs={1}>
-                        <>Start Time</>
-                      </Grid>
-                      <Grid item xs={1.25}>
-                        <>{cls.startTime}</>
-                      </Grid>
-                      <Grid item xs={1}>
-                        <>End Time</>
-                      </Grid>
-                      <Grid item xs={1.25}>
-                        <>{cls.endTime}</>
-                      </Grid>
-                    </>
-                  ))}
-              <Grid item xs={2}>
-                <>Training Date: </>
-              </Grid>
-              <Grid item xs={5}>
-                <>{addonService.formatDate(timetableDetail.trainingDate)}</>
-              </Grid>
-              <Grid item xs={2}>
-                <>Bird Skill Name: </>
-              </Grid>
-              <Grid item xs={10}>
-                <>{timetableDetail.birdSkillName}</>
-              </Grid>
-              <Grid item xs={2}>
-                <>Bird Skill Description: </>
-              </Grid>
-              <Grid item xs={10}>
-                <>{timetableDetail.birdSkillDescription}</>
-              </Grid>
-              <Grid item xs={2}>
-                <>Bird Name: </>
-              </Grid>
-              <Grid item xs={10}>
-                <>{timetableDetail.birdName}</>
-              </Grid>
-              <Grid item xs={2}>
-                <>Bird Species: </>
-              </Grid>
-              <Grid item xs={10}>
-                <>{timetableDetail.birdSpeciesName}</>
-              </Grid>
-              <Grid item xs={2}>
-                <>Bird Color: </>
-              </Grid>
-              <Grid item xs={10}>
-                <>{timetableDetail.birdColor}</>
-              </Grid>
-              <Grid item xs={2}>
-                <>Bird Picture: </>
-              </Grid>
-              <Grid item xs={10}>
-                <Img
-                  src={timetableDetail.birdPicture}
-                  alt="Bird Picture"
-                  style={{ width: "200px", height: "150px" }}
-                />
-              </Grid>
-            </Grid>
-            {timetableDetail.status == "NotYet" && (
-              <ThemeProvider theme={ochreTheme}>
-                <Button
-                  variant="contained"
-                  color="ochre"
-                  onClick={() => handleMarkSlotDone(timetableDetail.id)}
+          <div style={{ padding: "20px 40px" }}>
+            <table
+              style={{
+                border: "none",
+                padding: "0px",
+                borderCollapse: "separate",
+                borderSpacing: "1rem 0.5rem",
+                color: "#64645f",
+                fontSize: "1rem",
+                width: "100%",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                }}
+              >
+                {/* show bird details */}
+                <div
+                  style={{
+                    width: "100%",
+                  }}
                 >
-                  Mark Training Done
-                </Button>
-              </ThemeProvider>
-            )}
+                  <tr>
+                    {/* bird skill name */}
+                    <td style={{ fontSize: "1rem", fontWeight: 700 }}>
+                      <>Bird Skill Name: </>
+                    </td>
+                    <td item xs={10}>
+                      <>{timetableDetail.birdSkillName}</>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    {/* skill describe */}
+                    <td style={{ fontSize: "1rem", fontWeight: 700 }}>
+                      <>Bird Skill Description: </>
+                    </td>
+                    <td item xs={10}>
+                      <>{timetableDetail.birdSkillDescription}</>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    {/* bird name */}
+                    <td style={{ fontSize: "1rem", fontWeight: 700 }}>
+                      <>Bird Name: </>
+                    </td>
+                    <td item xs={10}>
+                      <>{timetableDetail.birdName}</>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    {/* species */}
+                    <td style={{ fontSize: "1rem", fontWeight: 700 }}>
+                      <>Bird Species: </>
+                    </td>
+                    <td item xs={10}>
+                      <>{timetableDetail.birdSpeciesName}</>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    {/* color bird */}
+                    <td style={{ fontSize: "1rem", fontWeight: 700 }}>
+                      <>Bird Color: </>
+                    </td>
+                    <td item xs={10}>
+                      <>{timetableDetail.birdColor}</>
+                    </td>
+                  </tr>
+                </div>
+
+                {/* show date, time and picture */}
+                <div
+                  style={{
+                    width: "100%",
+                  }}
+                >
+                  {slotList != null &&
+                    slotList
+                      .filter((cls) => cls.id == timetableDetail.slotId)
+                      .map((cls) => (
+                        <>
+                          <tr>
+                            <td style={{ fontSize: "1.2rem", fontWeight: 700 }}>
+                              <>Start Time</>
+                            </td>
+                            <td item xs={1.25}>
+                              <>{cls.startTime}</>
+                            </td>
+                          </tr>
+
+                          <tr>
+                            <td style={{ fontSize: "1.2rem", fontWeight: 700 }}>
+                              <>End Time</>
+                            </td>
+                            <td item xs={1.25}>
+                              <>{cls.endTime}</>
+                            </td>
+                          </tr>
+                        </>
+                      ))}
+                  {/* traning date */}
+                  <tr>
+                    <td style={{ fontSize: "1.2rem", fontWeight: 700 }}>
+                      <>Training Date: </>
+                    </td>
+                    <td item xs={5}>
+                      {addonService.formatDate(timetableDetail.trainingDate)}
+                    </td>
+                  </tr>
+                  {/* picture */}
+                  <tr
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      marginLeft: 16,
+                    }}
+                  >
+                    <td
+                      style={{
+                        fontSize: "1.2rem",
+                        fontWeight: 700,
+                        marginRight: 40,
+                      }}
+                    >
+                      <>Bird Picture: </>
+                    </td>
+                    <td>
+                      <Img
+                        src={timetableDetail.birdPicture}
+                        alt="Bird Picture"
+                        style={{ width: "200px", height: "150px" }}
+                      />
+                    </td>
+                  </tr>
+                </div>
+              </div>
+            </table>
+
+            <div
+              style={{
+                marginTop: 20,
+                display: "flex",
+                justifyContent: "flex-end",
+              }}
+            >
+              {timetableDetail.status == "NotYet" && (
+                <ThemeProvider theme={ochreTheme}>
+                  <Button
+                    variant="contained"
+                    color="success"
+                    onClick={() => handleMarkSlotDone(timetableDetail.id)}
+                  >
+                    Mark Training Done
+                  </Button>
+                </ThemeProvider>
+              )}
+            </div>
           </div>
         )}
       </div>
