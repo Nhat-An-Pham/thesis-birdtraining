543023db73d133605a563723db2495aaa682e4d0
BaoNTT <phamnhatan01@gmail.com>
2023-12-22 18:29:21 +0700

    Update TicketBillView.jsx

--- src/Management/customer/TicketBillView.jsx
+++ src/Management/customer/TicketBillView.jsx
@@ -50,160 +50,158 @@ const TicketBillView = ({
           </DialogTitle>
           <DialogContent>
             <section>
-<div style={{width:"450px"}}>
-                    <form
-                      style={{ width: "100%", display: "flex" }}
-                      onSubmit={FinishTicket}
-                      className="form"
-                      encType="multipart/form-data"
+              <div style={{ width: "450px" }}>
+                <form
+                  style={{ width: "100%", display: "flex", justifyContent: "center" }}
+                  onSubmit={FinishTicket}
+                  className="form"
+                  encType="multipart/form-data"
+                >
+                  <div
+                    style={{
+                      boxShadow:
+                        "0px 2px 4px 2px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+                      borderRadius: 5,
+                      width: "90%",
+                      height: "100%",
+                      padding: "20px",
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "1rem",
+                    }}
+                  >
+                    <div
+                      style={{
+                        boxShadow:"0px 2px 4px 2px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+                        borderRadius: 5,
+                        width: "100%",
+                        height: "100%",
+                        padding: "20px",
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "1rem",
+                      }}
                     >
-                      <div
+                      Billing Details
+                    </div>
+                    <div>
+                      <table
                         style={{
-                          boxShadow:
-                            "0px 2px 4px 2px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
-                          borderRadius: 5,
-                          width: "90%",
-                          height: "100%",
-                          padding: "20px",
-                          display: "flex",
-                          flexDirection: "column",
-                          gap: "1rem",
+                          border: "none",
+                          padding: "0px",
+                          borderCollapse: "separate",
+                          borderSpacing: "1rem 0.5rem",
+                          color: "#64645f",
+                          fontSize: "1rem",
+                          width: "100%",
                         }}
                       >
-                        <div
-                          style={{
-boxShadow:
-                              "0px 2px 4px 2px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
-                            borderRadius: 5,
-                            width: "100%",
-                            height: "100%",
-                            padding: "20px",
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: "1rem",
-                          }}
-                        >
-                          Billing Details
-                        </div>
-                        <div>
-                          <table
-                            style={{
-                              border: "none",
-                              padding: "0px",
-                              borderCollapse: "separate",
-                              borderSpacing: "1rem 0.5rem",
-                              color: "#64645f",
-                              fontSize: "1rem",
-                              width: "100%",
-                            }}
-                          >
+                        <tr>
+                          <td>Ticket Id</td>
+                          <td style={{ textAlign: "end" }}>
+                            {ticketDetail.id}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Customer</td>
+                          <td style={{ textAlign: "end" }}>
+                            {ticketDetail.customerName}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Service</td>
+                          <td style={{ textAlign: "end" }}>
+                            {ticketDetail.onlineOrOffline
+                              ? "Online"
+                              : "Offine"}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Status</td>
+                          <td style={{ textAlign: "end" }}>
+                            {ticketDetail.status}
+                          </td>
+                        </tr>
+                        {ticketDetail.onlineOrOffline === false ? (
+                          <>
                             <tr>
-                              <td>Ticket Id</td>
+                              <td>Distance</td>
                               <td style={{ textAlign: "end" }}>
-                                {ticketDetail.id}
+                                {ticketDetail.distance} Km
                               </td>
                             </tr>
                             <tr>
-                              <td>Customer</td>
+                              <td>Distance Price</td>
                               <td style={{ textAlign: "end" }}>
-                                {ticketDetail.customerName}
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>Service</td>
-                              <td style={{ textAlign: "end" }}>
-                                {ticketDetail.onlineOrOffline
-                                  ? "Online"
-                                  : "Offine"}
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>Status</td>
-                              <td style={{ textAlign: "end" }}>
-                                {ticketDetail.status}
-                              </td>
-                            </tr>
-                            {ticketDetail.onlineOrOffline === false ? (
-                              <>
-                                <tr>
-                                  <td>Distance</td>
-                                  <td style={{ textAlign: "end" }}>
-                                    {ticketDetail.distance} Km
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>Distance Price</td>
-                                  <td style={{ textAlign: "end" }}>
-                                    {ticketDetail.distancePriceCalculate === 0
-                                      ? 0
-                                      : ticketDetail.distancePriceCalculate
-                                      ? addonService.formatCurrency(
-                                          ticketDetail.distancePriceCalculate
-                                        )
-                                      : null}{" "}
-                                    VND
-                                  </td>
-                                </tr>
-                              </>
-                            ) : (
-                              <></>
-                            )}
-                            <tr>
-                              <td>Actual Price</td>
-                              <td style={{ textAlign: "end" }}>
-                                {ticketDetail.price +
-                                  ticketDetail.discountedPrice ===
-                                0
+                                {ticketDetail.distancePriceCalculate === 0
                                   ? 0
-                                  : ticketDetail.price
-                                  ? addonService.formatCurrency(
-                                      ticketDetail.price +
-                                        ticketDetail.discountedPrice
+                                  : ticketDetail.distancePriceCalculate
+                                    ? addonService.formatCurrency(
+                                      ticketDetail.distancePriceCalculate
                                     )
-                                  : null}{" "}
+                                    : null}{" "}
                                 VND
                               </td>
                             </tr>
-                            <tr>
-                              <td>Membership Rank</td>
-                              <td style={{ textAlign: "end" }}>
-                                {ticketDetail.membershipRank}
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>Discounted Price</td>
-                              <td style={{ textAlign: "end" }}>
-                                {ticketDetail.discountedPrice === 0
-                                  ? 0
-                                  : ticketDetail.discountedPrice
-                                  ? addonService.formatCurrency(
-                                      ticketDetail.discountedPrice
-                                    )
-                                  : null}{" "}
-                                VND
-                              </td>
-                            </tr>
-                            <tr style={{ fontSize: "1.4rem", fontWeight: 700 }}>
-                              <td>Total Price</td>
-                              <td style={{ textAlign: "end" }}>
-                                {ticketDetail.price === 0
-                                  ? 0
-                                  : ticketDetail.price
-                                  ? addonService.formatCurrency(
-                                      ticketDetail.price
-                                    )
-                                  : null}{" "}
-                                VND
-                              </td>
-                            </tr>
-                          </table>
-                        </div>
-                      </div>
+                          </>
+                        ) : (
+                          <></>
+                        )}
+                        <tr>
+                          <td>Actual Price</td>
+                          <td style={{ textAlign: "end" }}>
+                            {ticketDetail.price +
+                              ticketDetail.discountedPrice ===
+                              0
+                              ? 0
+                              : ticketDetail.price
+                                ? addonService.formatCurrency(
+                                  ticketDetail.price +
+                                  ticketDetail.discountedPrice
+                                )
+                                : null}{" "}
+                            VND
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Membership Rank</td>
+                          <td style={{ textAlign: "end" }}>
+                            {ticketDetail.membershipRank}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Discounted Price</td>
+                          <td style={{ textAlign: "end" }}>
+                            {ticketDetail.discountedPrice === 0
+                              ? 0
+                              : ticketDetail.discountedPrice
+                                ? addonService.formatCurrency(
+                                  ticketDetail.discountedPrice
+                                )
+                                : null}{" "}
+                            VND
+                          </td>
+                        </tr>
+                        <tr style={{ fontSize: "1.4rem", fontWeight: 700 }}>
+                          <td>Total Price</td>
+                          <td style={{ textAlign: "end" }}>
+                            {ticketDetail.price === 0
+                              ? 0
+                              : ticketDetail.price
+                                ? addonService.formatCurrency(
+                                  ticketDetail.price
+                                )
+                                : null}{" "}
+                            VND
+                          </td>
+                        </tr>
+                      </table>
                     </div>
-                  </form>
-                </div>
+                  </div>
+                </form>
+              </div>
             </section>
-          </DialogContent>
+          </DialogContent >
           <DialogActions>
             <Button
               sx={{ marginBottom: "20px" }}
@@ -215,7 +213,7 @@ boxShadow:
               Confirm Finish Appointment
             </Button>
           </DialogActions>
-        </Dialog>
+        </Dialog >
       ) : (
         <></>
       )}
