fc35bbcde4552e965ce86ac00942eceffef548d2
DThongLeon <hoangthong432001@gmail.com>
2023-12-23 01:21:08 +0700

    fix task UI from 88 to 90

--- src/pages/CourseDetailPage.jsx
+++ src/pages/CourseDetailPage.jsx
@@ -1,28 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import OnlinecourseService from '../services/onlinecourse.service';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { jwtDecode } from 'jwt-decode';
-import RawHTMLRenderer from '../Management/component/htmlRender/htmlRender';
-import addonService from '../services/addon.service';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import OnlinecourseService from "../services/onlinecourse.service";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { jwtDecode } from "jwt-decode";
+import RawHTMLRenderer from "../Management/component/htmlRender/htmlRender";
+import addonService from "../services/addon.service";
 
 function CourseDetailPage() {
-
   const { courseid } = useParams();
   const [selectedCourse, setSelectedCourse] = useState();
   const navigate = useNavigate();
 
-
-  const accessToken = JSON.parse(localStorage.getItem('user-token'))
+  const accessToken = JSON.parse(localStorage.getItem("user-token"));
   let userRole = null;
   if (accessToken) {
     userRole = jwtDecode(accessToken).role;
   }
 
-
   if (userRole === "Customer" || userRole === null) {
   } else {
-    navigate('/courses')
+    navigate("/courses");
   }
 
   //API Handler
@@ -30,13 +27,12 @@ function CourseDetailPage() {
     OnlinecourseService.getOnlineCourseById({ id: courseid })
       .then((res) => {
         console.log("Success get Course By Id: ", res.data);
-        setSelectedCourse(res.data)
+        setSelectedCourse(res.data);
       })
       .catch((e) => {
-        console.log("Fail Get Course By Id: ", e)
-      })
-  }, [])
-
+        console.log("Fail Get Course By Id: ", e);
+      });
+  }, []);
 
   // Content NAVBAR
   const [contentButton, setContentButton] = useState("1");
@@ -44,15 +40,17 @@ function CourseDetailPage() {
   const contentNav = [
     {
       id: "1",
-      name: "Overview"
-    }, {
+      name: "Overview",
+    },
+    {
       id: "2",
-      name: "Course Content"
-    }, {
+      name: "Course Content",
+    },
+    {
       id: 3,
-      name: "Instructor"
-    }
-  ]
+      name: "Instructor",
+    },
+  ];
 
   //FUNCTION
   const handleBuyButton = () => {
@@ -60,82 +58,130 @@ function CourseDetailPage() {
       const oclassid = selectedCourse.id;
       navigate(`/payment/online/${oclassid}`);
     } else {
-      navigate("/login")
+      navigate("/login");
     }
-  }
+  };
 
   const handleStudyButton = () => {
-    navigate(`/onlinecourse/study/${courseid}`)
-  }
-
+    navigate(`/onlinecourse/study/${courseid}`);
+  };
 
   return (
+    <div
+      style={{
+        minHeight: "76.5vh",
+        display: "flex",
+      }}
+    >
+      <div className="coursedetailpage">
+        {selectedCourse ? (
+          <div className="cdtp_container">
+            <div className="cdtp_wrapper">
+              <div className="cdtp_sidebar">
+                <h3 className="cdtp_sidebar-title">{selectedCourse.title} </h3>
+                {selectedCourse.status === "Completed" ? (
+                  <p style={{ color: "green", margin: 0 }}>(Completed)</p>
+                ) : null}
+                <p className="cdtp_sidebar-price">
+                  Price: {addonService.formatCurrency(selectedCourse.price)}$
+                </p>
+                {selectedCourse.status === "Unenrolled" ? (
+                  <button onClick={handleBuyButton}>Enroll Now</button>
+                ) : null}
+                {selectedCourse.status === "Enrolled" ||
+                selectedCourse.status === "Completed" ? (
+                  <button onClick={handleStudyButton}>Study</button>
+                ) : null}
+                {selectedCourse.status === "Completed" ? (
+                  <>
+                    <Link
+                      to={`/onlinecourse/certificate/${selectedCourse.id}`}
+                      style={{ color: "black" }}
+                    >
+                      View Certificate
+                    </Link>
+                  </>
+                ) : null}
 
-    <div className='coursedetailpage'>
-      {selectedCourse ?
-        <div className='cdtp_container'>
-          <div className='cdtp_wrapper'>
-            <div className='cdtp_sidebar'>
-              <h3 className='cdtp_sidebar-title'>{selectedCourse.title} </h3>
-              {selectedCourse.status === "Completed" ?
-                <p style={{ color: "green", margin: 0 }}>(Completed)</p>
-                : null}
-              <p className='cdtp_sidebar-price'>Price: {addonService.formatCurrency(selectedCourse.price)}$</p>
-              {selectedCourse.status === "Unenrolled" ? <button onClick={handleBuyButton}>Enroll Now</button>
-                : null}
-              {selectedCourse.status === "Enrolled" || selectedCourse.status === "Completed" ? <button onClick={handleStudyButton}>Study</button> : null}
-              {selectedCourse.status === "Completed" ?
-                <>
-                  <Link to={`/onlinecourse/certificate/${selectedCourse.id}`}
-                    style={{ color: "black" }}>View Certificate</Link>
-                </>
-                : null}
-
-              <div className='cdtp_sidebar-skill'>
-                <div className='cdtp_sidebar-skill-skillbox'>
-                  <p className='cdtp_sidebar-skill-skillbox-first'>Skill level</p>
-                  <p className='cdtp_sidebar-skill-skillbox-second'>Basic</p>
-                </div>
-                <div className='cdtp_sidebar-skill-skillbox'>
-                  <p className='cdtp_sidebar-skill-skillbox-first'>Enrolled</p>
-                  <p className='cdtp_sidebar-skill-skillbox-second'>100</p>
-                </div>
-
-              </div>
-              <div className='cdtp_sidebar-footer'>
-                <p>For Details About The Course</p>
-                <button>Call Us: (+84) 90 456 0264</button>
-              </div>
-            </div>
-            <div className='cdtp_content'>
-              <div className='cdtp_content-descr'>
-                <section className='cdpt_content_descr-box'>
-                  <h4>What You'll Learn</h4>
-                  <p><RawHTMLRenderer htmlContent={selectedCourse.shortDescription} /></p>
-                </section>
-                <section className='cdpt_content_descr-box'>
-                  <h4>Some Images Of The Course</h4>
-                  <img alt='Course Picture' src={selectedCourse.picture}></img>
-                </section>
-                <section className='cdpt_content_descr-box'>
-                  <h4>Requirements</h4>
-                  <div className='cdpt_content_descr_box-req-body'>
-                    <FontAwesomeIcon icon="fa-solid fa-check" style={{ color: "#2fda3a", }} />
-                    <p>Become an advanced, confident, and modern JavaScript developer from scratch.</p>
-                    <FontAwesomeIcon icon="fa-solid fa-check" style={{ color: "#2fda3a", }} />
-                    <p> Have an intermediate skill level of Python programming.</p>
-                    <FontAwesomeIcon icon="fa-solid fa-check" style={{ color: "#2fda3a", }} />
-                    <p> Have a portfolio of various data analysis projects.</p>
-                    <FontAwesomeIcon icon="fa-solid fa-check" style={{ color: "#2fda3a", }} />
-                    <p> Use the numpy library to create and manipulate arrays.</p>
+                <div className="cdtp_sidebar-skill">
+                  <div className="cdtp_sidebar-skill-skillbox">
+                    <p className="cdtp_sidebar-skill-skillbox-first">
+                      Skill level
+                    </p>
+                    <p className="cdtp_sidebar-skill-skillbox-second">Basic</p>
                   </div>
-                </section>
+                  <div className="cdtp_sidebar-skill-skillbox">
+                    <p className="cdtp_sidebar-skill-skillbox-first">
+                      Enrolled
+                    </p>
+                    <p className="cdtp_sidebar-skill-skillbox-second">100</p>
+                  </div>
+                </div>
+                <div className="cdtp_sidebar-footer">
+                  <p>For Details About The Course</p>
+                  <button>Call Us: (+84) 90 456 0264</button>
+                </div>
+              </div>
+              <div className="cdtp_content">
+                <div className="cdtp_content-descr">
+                  <section className="cdpt_content_descr-box">
+                    <h4>What You'll Learn</h4>
+                    <p>
+                      <RawHTMLRenderer
+                        htmlContent={selectedCourse.shortDescription}
+                      />
+                    </p>
+                  </section>
+                  <section className="cdpt_content_descr-box">
+                    <h4>Some Images Of The Course</h4>
+                    <img
+                      alt="Course Picture"
+                      src={selectedCourse.picture}
+                    ></img>
+                  </section>
+                  <section className="cdpt_content_descr-box">
+                    <h4>Requirements</h4>
+                    <div className="cdpt_content_descr_box-req-body">
+                      <FontAwesomeIcon
+                        icon="fa-solid fa-check"
+                        style={{ color: "#2fda3a" }}
+                      />
+                      <p>
+                        Become an advanced, confident, and modern JavaScript
+                        developer from scratch.
+                      </p>
+                      <FontAwesomeIcon
+                        icon="fa-solid fa-check"
+                        style={{ color: "#2fda3a" }}
+                      />
+                      <p>
+                        {" "}
+                        Have an intermediate skill level of Python programming.
+                      </p>
+                      <FontAwesomeIcon
+                        icon="fa-solid fa-check"
+                        style={{ color: "#2fda3a" }}
+                      />
+                      <p>
+                        {" "}
+                        Have a portfolio of various data analysis projects.
+                      </p>
+                      <FontAwesomeIcon
+                        icon="fa-solid fa-check"
+                        style={{ color: "#2fda3a" }}
+                      />
+                      <p>
+                        {" "}
+                        Use the numpy library to create and manipulate arrays.
+                      </p>
+                    </div>
+                  </section>
+                </div>
               </div>
             </div>
-
           </div>
-        </div> : null
-      }
+        ) : null}
+      </div>
     </div>
   );
 }
--- src/pages/WorkshopPage.jsx
+++ src/pages/WorkshopPage.jsx
@@ -1,165 +1,214 @@
-import React from 'react'
-import Cards from '../components/cards/WorkshopClassListCards'
-import { Link } from 'react-router-dom'
-import { useState, useEffect } from "react"
-import WorkshopService from '../services/workshop.service'
+import React from "react";
+import Cards from "../components/cards/WorkshopClassListCards";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import WorkshopService from "../services/workshop.service";
 // import workshops from '../assets/fakedb/workshops'
-import dateFormat from 'dateformat';
-import { Typography } from '@mui/material';
-import StopIcon from '@mui/icons-material/Stop';
-import RawHTMLRenderer from '../Management/component/htmlRender/htmlRender';
-
+import dateFormat from "dateformat";
+import { Typography } from "@mui/material";
+import StopIcon from "@mui/icons-material/Stop";
+import RawHTMLRenderer from "../Management/component/htmlRender/htmlRender";
 
 const Workshop = () => {
+  const accessToken = JSON.parse(localStorage.getItem("user-token"));
 
-    const accessToken = JSON.parse(localStorage.getItem("user-token"))
+  const [workshopList, setWorkshopList] = useState([]);
+  const [registeredClasses, setRegisteredClasses] = useState([]);
+  //take first 4 workshops
+  const sliceWorkshop = workshopList.slice(0, 4);
+  const token = JSON.parse(localStorage.getItem("user-token"));
+  useEffect(() => {
+    WorkshopService.getWorkshopList()
+      .then((res) => {
+        // console.log("success workshop list test", res.data);
+        setWorkshopList(res.data.slice(0, 4));
+      })
+      .catch((e) => console.log("fail workshop list test", e));
+    WorkshopService.getRegisterdClasses()
+      .then((res) => {
+        console.log(res.data);
+        setRegisteredClasses(res.data);
+      })
+      .catch((e) => {
+        console.log("Cannot Get Registered Classes", e);
+      });
+  }, []);
 
-    const [workshopList, setWorkshopList] = useState([]);
-    const [registeredClasses, setRegisteredClasses] = useState([]);
-    //take first 4 workshops
-    const sliceWorkshop = workshopList.slice(0, 4)
-    const token = JSON.parse(localStorage.getItem("user-token")
-    )
-    useEffect(() => {
-        WorkshopService
-            .getWorkshopList()
-            .then((res) => {
-                // console.log("success workshop list test", res.data);
-                setWorkshopList(res.data.slice(0, 4));
-            })
-            .catch((e) => console.log("fail workshop list test", e));
-        WorkshopService.getRegisterdClasses()
-            .then((res) => {
-                console.log(res.data)
-                setRegisteredClasses(res.data);
-            })
-            .catch((e) => {
-                console.log("Cannot Get Registered Classes", e)
-            })
+  const [isDivVisible, setIsDivVisible] = useState(false);
+  const handleButtonOpenDivClick = () => {
+    setIsDivVisible(true);
+  };
+  const handleCloseDiv = () => {
+    setIsDivVisible(false);
+  };
+  const [selectedClass, setSelectedClass] = useState(null);
+  const handleWorkshopClick = (classesssss) => {
+    setSelectedClass(classesssss);
+    // console.log(selectedClass)
+  };
 
-    }, []);
+  const twoFunctionOnClick = (classesssss) => {
+    handleWorkshopClick(classesssss);
+    handleButtonOpenDivClick();
+  };
 
-    const [isDivVisible, setIsDivVisible] = useState(false);
-    const handleButtonOpenDivClick = () => {
-        setIsDivVisible(true);
-    };
-    const handleCloseDiv = () => {
-        setIsDivVisible(false);
-    };
-    const [selectedClass, setSelectedClass] = useState(null);
-    const handleWorkshopClick = (classesssss) => {
-        setSelectedClass(classesssss);
-        // console.log(selectedClass)
-    };
+  console.log("workshop.description", sliceWorkshop);
 
-    const twoFunctionOnClick = (classesssss) => {
-        handleWorkshopClick(classesssss);
-        handleButtonOpenDivClick();
-    }
-
-
-
-    return (
-        <div className='workshopspage'>
-            {/* carousel */}
-            {isDivVisible && (
-                // Popup
-                <div className='wclpdiv-background'>
-                    <div className='wclpdiv-container'>
-                        <h2 className='wclpdiv_section wclpdiv_section-title' style={{ fontSize: "25px" }}>{workshopList.title}</h2>
-                        <p className='wclpdiv_section wclpdiv_section-title' style={{ fontSize: "20px" }}>Date: <span style={{ color: "red", fontSize: "20px" }}>{dateFormat(selectedClass.date, "mmmm dS, yyyy")}</span></p>
-                        <div className='wclpdiv_section wclpdiv_section-mapping'>
-                            {selectedClass.classSlots.map((workshopClass) => (
-                                <div className='wclpdiv_section_mapping-container'>
-                                    <div className='wclpdiv_section_mapping-content' key={workshopClass.id}>
-                                        <p className='wclpdiv_content wclpdiv_content-detail '>Description: <span><RawHTMLRenderer htmlContent={workshopClass.detail} /></span></p>
-                                        <p className='wclpdiv_content wclpdiv_content-trainer'>Trainer: {workshopClass.trainer.name}</p>
-                                        <p className='wclpdiv_content wclpdiv_content-registered'>Start-Time: {workshopClass.startTime}/End-Time: {workshopClass.endTime}</p>
-                                        -----------------------
-                                    </div>
-                                    {workshopClass.trainer.avatar ?
-                                        <img src={workshopClass.trainer.avatar}></img>
-                                        : null}
-                                </div>
-                            ))}
-                        </div>
-                        <div className='wclpdiv_section wclpdiv_section-button'>
-                            <button className='wclpdiv_section_button-close' onClick={handleCloseDiv}>Close</button>
-                        </div>
-                    </div>
-                </div>
-            )}
-            <div className='workshoppage_section workshoppage_section-carousel'>
-                <div className='workshoppage_carousel'>
-                    <div className='workshoppage_carousel_section workshoppage_carousel_section-text'>
-                        <h1>COME TO OUR WORKSHOP</h1>
-                        <h5>Event every weekend</h5>
-                    </div>
-                    <div class="workshoppage_carousel_section-curve"></div>
-
-                    <div className='workshoppage_carousel_section workshoppage_carousel_section-button'>
-                        {!token &&
-                            <Link to='/signup'><button>Join for free</button></Link>
-                        }
-                    </div>
-                    <div className='workshoppage_carousel_section ocp_carousel_section-video'>
-                        <img src={require("../assets/pages/ocp/ocp_carousel.jpg")} alt='' />
-                    </div>
-                </div>
-            </div>
-            {/* events */}
-            <div className='workshoppage_section workshoppage_section-events'>
-                <div className='workshoppageevents_elements workshoppageevents_elements-title '>
-                    <p>
-                        Top of our categories
+  return (
+    <div className="workshopspage">
+      {/* carousel */}
+      {isDivVisible && (
+        // Popup
+        <div className="wclpdiv-background">
+          <div className="wclpdiv-container">
+            <h2
+              className="wclpdiv_section wclpdiv_section-title"
+              style={{ fontSize: "25px" }}
+            >
+              {workshopList.title}
+            </h2>
+            <p
+              className="wclpdiv_section wclpdiv_section-title"
+              style={{ fontSize: "20px" }}
+            >
+              Date:{" "}
+              <span style={{ color: "red", fontSize: "20px" }}>
+                {dateFormat(selectedClass.date, "mmmm dS, yyyy")}
+              </span>
+            </p>
+            <div className="wclpdiv_section wclpdiv_section-mapping">
+              {selectedClass.classSlots.map((workshopClass) => (
+                <div className="wclpdiv_section_mapping-container">
+                  <div
+                    className="wclpdiv_section_mapping-content"
+                    key={workshopClass.id}
+                  >
+                    <p className="wclpdiv_content wclpdiv_content-detail ">
+                      Description:{" "}
+                      <span>
+                        <RawHTMLRenderer htmlContent={workshopClass.detail} />
+                      </span>
                     </p>
-                    <h2>
-                        Explore our popular Workshops
-                    </h2>
+                    <p className="wclpdiv_content wclpdiv_content-trainer">
+                      Trainer: {workshopClass.trainer.name}
+                    </p>
+                    <p className="wclpdiv_content wclpdiv_content-registered">
+                      Start-Time: {workshopClass.startTime}/End-Time:{" "}
+                      {workshopClass.endTime}
+                    </p>
+                    -----------------------
+                  </div>
+                  {workshopClass.trainer.avatar ? (
+                    <img src={workshopClass.trainer.avatar}></img>
+                  ) : null}
                 </div>
-                <div className='workshoppageevents_elements workshoppageevents_elements-cards'>
-                    {sliceWorkshop.map((workshop) => (
-                        <Cards id={workshop.id} title={workshop.title} key={workshop.id}
-                            thumbnail={workshop.picture.split(",")[0]} shortdescr={workshop.description}
-                            price={workshop.price} >
-                        </Cards>
-                    ))}
-                </div>
-                {accessToken ?
-                    <Link to='/setting' style={{ color: "grey" }}>Click Here To View Your Workshop</Link>
-                    : null}
-                <div className='workshoppageevents_elements-button'>
-                    <Link to="/workshopslist">VIEW MORE</Link>
-                </div>
+              ))}
             </div>
-            <div className='workshoppage_section workshoppage_section-registered'>
-                <div className='workshoppageevents_elements workshoppageevents_elements-title '>
-                    <h2>
-                        Your Registered Workshop Classes
-                    </h2>
-                </div>
-                <div className='workshoppageevents_elements workshoppageevents_elements-cards' style={{ width: "100%" }}>
-                    {registeredClasses ?
-                        <div className='wclp_section wclp_section-cards'>
-                            {registeredClasses.map((classeses, index) => (
-                                <Link key={index} className='wclp_card-container'
-                                    onClick={() => twoFunctionOnClick(classeses)}
-                                >
-                                    <div className='wclp_card_section wclp_card_section-bottom'>
-                                        <h2>Location: {classeses.location}</h2>
-                                        <h5>Register Start Date: {dateFormat(classeses.startTime, "mmmm dS, yyyy")}</h5><br />
-                                        <p>Register End Date: <span> {dateFormat(classeses.registerEndDate, "mmmm dS, yyyy")}</span></p>
-                                        {/* <p>Registered: {classeses.registered.registered}/{classeses.registered.maximum}</p> */}
-                                        {/* Hỏi về cái class status có bao nhiêu status  */}
-                                    </div>
-                                </Link>
-                            ))}
-                        </div>
-                        : <><Typography>This Workshop has no class yet</Typography></>}
-                </div>
+            <div className="wclpdiv_section wclpdiv_section-button">
+              <button
+                className="wclpdiv_section_button-close"
+                onClick={handleCloseDiv}
+              >
+                Close
+              </button>
             </div>
-        </div >
-    )
-}
+          </div>
+        </div>
+      )}
+      <div className="workshoppage_section workshoppage_section-carousel">
+        <div className="workshoppage_carousel">
+          <div className="workshoppage_carousel_section workshoppage_carousel_section-text">
+            <h1>COME TO OUR WORKSHOP</h1>
+            <h5>Event every weekend</h5>
+          </div>
+          <div class="workshoppage_carousel_section-curve"></div>
 
-export default Workshop
+          <div className="workshoppage_carousel_section workshoppage_carousel_section-button">
+            {!token && (
+              <Link to="/signup">
+                <button>Join for free</button>
+              </Link>
+            )}
+          </div>
+          <div className="workshoppage_carousel_section ocp_carousel_section-video">
+            <img src={require("../assets/pages/ocp/ocp_carousel.jpg")} alt="" />
+          </div>
+        </div>
+      </div>
+      {/* events */}
+      <div className="workshoppage_section workshoppage_section-events">
+        <div className="workshoppageevents_elements workshoppageevents_elements-title ">
+          <p>Top of our categories</p>
+          <h2>Explore our popular Workshops</h2>
+        </div>
+        <div className="workshoppageevents_elements workshoppageevents_elements-cards">
+          {sliceWorkshop.map((workshop) => (
+            <Cards
+              id={workshop.id}
+              title={workshop.title}
+              key={workshop.id}
+              thumbnail={workshop.picture.split(",")[0]}
+              shortdescr={workshop.description}
+              price={workshop.price}
+            ></Cards>
+          ))}
+        </div>
+        {accessToken ? (
+          <Link to="/setting" style={{ color: "grey" }}>
+            Click Here To View Your Workshop
+          </Link>
+        ) : null}
+        <div className="workshoppageevents_elements-button">
+          <Link to="/workshopslist">VIEW MORE</Link>
+        </div>
+      </div>
+      {/* registerd class */}
+      <div className="workshoppage_section workshoppage_section-registered">
+        <div
+          className="workshoppageevents_elements workshoppageevents_elements-title "
+          style={{ width: "100%" }}
+        >
+          <h2>Your Registered Workshop Classes</h2>
+        </div>
+        <div
+          className="workshoppageevents_elements workshoppageevents_elements-cards"
+          style={{ width: "100%" }}
+        >
+          {registeredClasses ? (
+            <div className="wclp_section wclp_section-cards">
+              {registeredClasses.map((classeses, index) => (
+                <Link
+                  key={index}
+                  className="wclp_card-container"
+                  onClick={() => twoFunctionOnClick(classeses)}
+                >
+                  <div className="wclp_card_section wclp_card_section-bottom">
+                    <h2>Location: {classeses.location}</h2>
+                    <h5>
+                      Register Start Date:{" "}
+                      {dateFormat(classeses.startTime, "mmmm dS, yyyy")}
+                    </h5>
+                    <br />
+                    <p>
+                      Register End Date:{" "}
+                      <span>
+                        {" "}
+                        {dateFormat(classeses.registerEndDate, "mmmm dS, yyyy")}
+                      </span>
+                    </p>
+                    {/* <p>Registered: {classeses.registered.registered}/{classeses.registered.maximum}</p> */}
+                    {/* Hỏi về cái class status có bao nhiêu status  */}
+                  </div>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <Typography>This Workshop has no class yet</Typography>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Workshop;
