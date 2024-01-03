bb6fecf46784fa3f558f24ffd662039a331a0c98
WilliamTrung <williamthanhtrungq2@gmail.com>
2024-01-03 18:54:43 +0700

    fix error on sign up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,77 +1,134 @@
-import { React, useState } from 'react'
-import { Link } from 'react-router-dom'
-import AuthService from '../services/auth.service';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import AuthService from "../services/auth.service";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const SignUp = () => {
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
-    const [phone, setPhone] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [passwordMatch, setPasswordMatch] = useState(true);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordMatch, setPasswordMatch] = useState(true);
 
-    const [err, setErr] = useState("");
-    const navigator = useNavigate();
+  const [err, setErr] = useState("");
+  const navigator = useNavigate();
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    }
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    }
-    const handlePhoneChange = (event) => {
-        setPhone(event.target.value);
-    }
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    }
-    const handleConfirmPasswordChange = (event) => {
-        setConfirmPassword(event.target.value);
-    };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
 
-    const confirmPasswordFunction = () => {
-        const passwordMatch = password === confirmPassword;
-        setPasswordMatch(passwordMatch);
-    };
+  const confirmPasswordFunction = () => {
+    const passwordMatch = password === confirmPassword;
+    setPasswordMatch(passwordMatch);
+  };
 
-    const handleSave = () => {
-        if (password === confirmPassword) {
-            AuthService.register({ name: name, password: password, email: email, phoneNumber: phone })
-                .then(() => {
-                    alert("Successfully Registered")
-                    navigator('/home');
-                })
-                .catch(() => {
-                    setErr("Existed Email Or Phone Number")
-                });
-        }
+  const handleSave = () => {
+    if (password === confirmPassword) {
+      AuthService.register({
+        name: name,
+        password: password,
+        email: email,
+        phoneNumber: phone,
+      })
+        .then(() => {
+          alert("Successfully Registered");
+          navigator("/home");
+        })
+        .catch((e) => {
+          if (e.response?.data?.errors) {
+            let errors = e.response.data.errors;
+            let errorMsg = "";
+            if (errors.Email) {
+              errorMsg = errorMsg.concat(errors?.Email[0]);
+            }
+            if (errors.PhoneNumber) {
+              if (errorMsg !== "") {
+                errorMsg = errorMsg.concat("<br/>");
+              }
+              errorMsg = errorMsg.concat(errors?.PhoneNumber);
+            }
+            console.log(errorMsg);
+            setErr(errorMsg);
+          } else {
+            setErr("Existed Email Or Phone Number");
+          }
+        });
     }
-    return (
-        <div className='signuppage'>
-            <section className='signup-container'>
-                <h1 className='signup_section signup_section-title'>Sign Up</h1>
-                <div className='signup_section signup_section-input'>
-                    <input placeholder='Enter your Email' value={email} onChange={handleEmailChange} ></input>
-                    <input placeholder='Enter your Name' value={name} onChange={handleNameChange}></input>
-                    <input placeholder='Enter your Phone Number' value={phone} onChange={handlePhoneChange}></input>
-                    <input type='password' placeholder='Password' value={password} onChange={handlePasswordChange}></input>
-                    <input type='password' placeholder='Confirm your Password' value={confirmPassword} onChange={handleConfirmPasswordChange}
-                        onBlur={confirmPasswordFunction}></input>
-                    {!passwordMatch && <p style={{ color: 'red' }}>Passwords do not match.</p>}
-                    <p style={{ 'color': 'red' }}>
+  };
+  return (
+    <div className="signuppage">
+      <section className="signup-container">
+        <h1 className="signup_section signup_section-title">Sign Up</h1>
+        <div className="signup_section signup_section-input">
+          <input
+            placeholder="Enter your Email"
+            value={email}
+            onChange={handleEmailChange}
+          ></input>
+          <input
+            placeholder="Enter your Name"
+            value={name}
+            onChange={handleNameChange}
+          ></input>
+          <input
+            placeholder="Enter your Phone Number"
+            value={phone}
+            onChange={handlePhoneChange}
+          ></input>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          ></input>
+          <input
+            type="password"
+            placeholder="Confirm your Password"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            onBlur={confirmPasswordFunction}
+          ></input>
+          {!passwordMatch && (
+            <p style={{ color: "red" }}>Passwords do not match.</p>
+          )}
+          {err?.split("<br/>").map((e) => (
+            <p style={{ color: "red", marginBottom : 0 }}>{e}</p>
+          ))}
+          {/* <p style={{ 'color': 'red' }} >
                         {err}
-                    </p>
-                </div>
-                <button className='signup_section signup_section-button' onClick={() => handleSave()}>Sign Up</button>
-                <div className='signup_section signup_section-link'>
-                    <p><Link to="/home">Back to home page</Link></p>
-                    <p>Already have an account? <Link to="/login"> click here</Link></p>
-                </div>
-            </section>
+                    </p> */}
         </div>
-    )
-}
+        <button
+          className="signup_section signup_section-button"
+          onClick={() => handleSave()}
+        >
+          Sign Up
+        </button>
+        <div className="signup_section signup_section-link">
+          <p>
+            <Link to="/home">Back to home page</Link>
+          </p>
+          <p>
+            Already have an account? <Link to="/login"> click here</Link>
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
